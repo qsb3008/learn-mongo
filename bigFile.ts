b5acae795b9bfff44db703c918fd982e3acd01dd
光天化日之下 <qsb3008@gmail.com>
2025-02-16 15:31:16 +0800

    feat: 权限管理 casl 练习

--- bigFile.ts
+++ bigFile.ts
@@ -2,9 +2,26 @@
  * 生成大文件：100M左右
  * 生成方式：循环写入
  */
-const fs = require("fs");
-const file = fs.createWriteStream("./big.file");
-for (let i = 0; i <= 2e6; i++) {
-  file.write("Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n");
-}
-file.end();
+// const fs = require("fs");
+// const file = fs.createWriteStream("./big.file");
+// for (let i = 0; i <= 2e6; i++) {
+//   file.write("Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n");
+// }
+// file.end();
+import * as fs from "fs";
+import * as path from "path";
+
+const filePath = path.join(__dirname, "bigFile.txt");
+const fileStream = fs.createWriteStream(filePath);
+
+const generateBigFile = (sizeInMB: number) => {
+  const chunkSize = 1024 * 1024; // 1MB
+  const totalChunks = sizeInMB;
+  for (let i = 0; i < totalChunks; i++) {
+    const chunk = Buffer.alloc(chunkSize, "a");
+    fileStream.write(chunk);
+  }
+  fileStream.end();
+};
+
+generateBigFile(100);
